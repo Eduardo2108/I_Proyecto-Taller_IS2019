c8db9437a1264130a51efc37fb2fd139a39eb4cf
Eduardo Granados <eduardo212792@gmail.com>
2019-04-05 01:16:20 -0600

    Separar Player aparte

--- classes.py
+++ classes.py
@@ -1,82 +1,12 @@
 # Libraries
 import pygame
 import time
+from player import *
 # Settings
 pygame.init()
-transColor = pygame.Color(100, 100, 100)
+
 
 # Clase que define los parametros del jugador principal (mario)
-class Player:
-    def __init__(self, x, y): #crea el jugador en una posicion x,y
-        self.spriteSheet = pygame.image.load('data/playerSheet.png') #carga el sprite de mario...
-
-        # Sprite que se usa cuando quieto
-        self.spriteSheet.set_clip(27, 37, 75, 125)
-        self.standing = self.spriteSheet.subsurface(self.spriteSheet.get_clip()).convert()
-        self.standing.set_colorkey(transColor)
-
-        # los sprites que se usan cuando hay movimiento hacia los lados
-        self.spriteSheet.set_clip(25, 205, 75, 125)
-        walking1 = self.spriteSheet.subsurface(self.spriteSheet.get_clip()).convert()
-        walking1.set_colorkey(transColor)
-        self.spriteSheet.set_clip(155, 201, 75, 125)
-        walking2 = self.spriteSheet.subsurface(self.spriteSheet.get_clip()).convert()
-        walking2.set_colorkey(transColor)
-        self.spriteSheet.set_clip(285, 198, 75, 125)
-        walking3 = self.spriteSheet.subsurface(self.spriteSheet.get_clip()).convert()
-        walking3.set_colorkey(transColor)
-        self.spriteSheet.set_clip(416, 205, 75, 125)
-        walking4 = self.spriteSheet.subsurface(self.spriteSheet.get_clip()).convert()
-        walking4.set_colorkey(transColor)
-        self.walking = [walking1, walking2, walking3, walking4]
-
-
-        # estados posibles de mario
-        self.sprite = self.standing
-        self.sprite_index = 0
-        self.rect = self.sprite.get_rect() #rectangulo para colisiones
-        self.rect.topleft = (x, y)
-        self.action = 'standing' #cuando no camina
-        self.state = 'right'#cuando si camina
-        self.jumpTime = 1.5
-        self.tag = 'player' # etiqueta, que lo identifica en el juego
-        self.falling = True
-        self.jump = 0
-
-    def __update__(self):
-        if self.action == 'standing': #le dice que hacer si esta quieto
-            if self.state == 'left':
-                self.sprite = self.standing
-                self.sprite = pygame.transform.flip(self.standing, True, False)
-            elif self.state == 'right':
-                self.sprite = self.standing
-        elif self.action == 'walking': # le dice que hacer si camina o no
-            if self.state == 'left':
-                self.rect.left -= 12
-                self.sprite = self.__get_sprite__(self.walking)
-
-            elif self.state == 'right':
-                self.rect.right += 12
-                self.sprite = self.__get_sprite__(self.walking)
-                self.sprite = pygame.transform.flip(self.sprite, True, False)
-
-    def __draw__(self, frame): #dibuja a mario en la pantalla segun si camina o no
-        pygame.draw.rect(frame, (255, 0, 0), self.rect)
-        if self.action == 'standing':
-            frame.blit(self.sprite, (self.rect.left, self.rect.top))
-
-        elif self.action == 'walking':
-            if self.state == 'left':
-                frame.blit(self.sprite, (self.rect.left, self.rect.top + 10))
-
-            elif self.state == 'right':
-                frame.blit(self.sprite, (self.rect.left - 16, self.rect.top + 10))
-
-    def __get_sprite__(self, sprites):
-        self.sprite_index += 1
-        if self.sprite_index >= (len(sprites)):
-            self.sprite_index = 0
-        return sprites[self.sprite_index]
 
 # clase que crea los parametros de las plataformas
 class plataformas:
